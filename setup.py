340a4a0a8b4374a0db2974e8f55bb7fc6c5d839f
DSimonne <davidsimonne@gmail.com>
2021-06-18 06:25:51 +0200

    Reorganize to make a package

---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("phdutils/README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="phdutils",
+    version="0.0.1",
+    description="Python package for BCDI and SXRD",
+    author="David Simonne",
+    author_email="david.simonne@synchrotron-soleil.fr",
+    url="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+
+    keywords = "XANES GUI lmfit widgets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+	include_package_data=True,
+    python_requires='>=3.6',
+    install_requires=[
+    "numpy",
+    "pandas",
+    "matplotlib",
+    "ipywidgets",
+    "ipython",
+    "scipy",
+    "lmfit",
+    "xrayutilities",
+    "tables"]
+)
