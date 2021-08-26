622568d6559b57afdceab116785ca12cf81233bf
Ravenne Linux <davidsimonne@gmail.com>
2021-08-26 10:13:57 +0200

    submodule mask

--- phdutils/bcdi/__init__.py
+++ phdutils/bcdi/__init__.py
@@ -7,4 +7,4 @@
 Welcome to phdutils.bcdi, there is one submodule used for the gui. The terminal scripts used for quick analysis are in the terminal_scripts folder
 """
 
-__all__ = ["gui", "plot", "read_vtk"]
+__all__ = ["gui", "mask, ""plot", "read_vtk"]
---
+++ phdutils/bcdi/mask/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+"""
+Welcome to phdutils.bcdi.gui"""
+
+__all__ = ["create_mask", "extract_mask", "gaussian_filter_mask"]
