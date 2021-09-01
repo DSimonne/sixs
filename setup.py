29e80fc964de3a37d29519522af8fd6eb6d17fec
Ravenne Linux <davidsimonne@gmail.com>
2021-09-01 16:48:24 +0200

    Add non .py files with datafiles in setup because MANIFEST has problems on jupyter hub

--- setup.py
+++ setup.py
@@ -9,29 +9,17 @@ setuptools.setup(
     description="Python package for BCDI and SXRD",
     author="David Simonne",
     author_email="david.simonne@synchrotron-soleil.fr",
-    url="",
-    # scripts=[
-    #     'phdutils/bcdi/terminal_scripts/compute_q.py',
-    #     'phdutils/bcdi/terminal_scripts/correct_angles_detector.py',
-    #     'phdutils/bcdi/terminal_scripts/facet_strain.py',
-    #     'phdutils/bcdi/terminal_scripts/make_support.py',
-    #     'phdutils/bcdi/terminal_scripts/movetodir.py',
-    #     'phdutils/bcdi/terminal_scripts/preprocess_bcdi_merlin_ortho.py',
-    #     'phdutils/bcdi/terminal_scripts/print_pos.py',
-    #     'phdutils/bcdi/terminal_scripts/quick_phase_retrieval.sh',
-    #     'phdutils/bcdi/terminal_scripts/quick_process.sh',
-    #     'phdutils/bcdi/terminal_scripts/rotate.py',
-    #     'phdutils/bcdi/terminal_scripts/slice_cxi.py',
-    #     'phdutils/bcdi/terminal_scripts/strain-SIXS_merlin.py',
-    #     'phdutils/bcdi/terminal_scripts/Sync_from_id01.sh',
-    #     'phdutils/bcdi/terminal_scripts/Sync_to_id01.sh',
-    #     ],
+    data_files=[('', ["phdutils/bcdi/pynx_run.txt",
+                      "phdutils/bcdi/CompareFacetsEvolution.ipynb", 
+                      "phdutils/bcdi/PhasingNotebook.ipynb", 
+                      "licence.txt"
+                     ])],
+    url="https://github.com/DSimonne/PhDUtils/tree/master",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-
     keywords = "BCDI SXRD",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -45,7 +33,6 @@ setuptools.setup(
         "ipywidgets",
         "ipython",
         "scipy",
-        "lmfit",
         "xrayutilities",
         "tables"
         ]
