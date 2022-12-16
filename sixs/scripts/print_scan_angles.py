9177ae2db6b1a047d4cbb782adf2dc012c29453a
SixS3 David <david.simonne@synchrotron-soleil.fr>
2022-12-16 17:53:40 +0100

    Add hkl

--- sixs/scripts/print_scan_angles.py
+++ sixs/scripts/print_scan_angles.py
@@ -6,7 +6,7 @@ import glob
 from sixs.experiments import coherence as coh
 import tables as tb
 from prettytable import PrettyTable
-
+import numpy as np
 
 def get_scan_angles(filename):
     """
@@ -15,16 +15,19 @@ def get_scan_angles(filename):
     :param filename: string, absolute path to data
     """
     with tb.open_file(filename) as f:
+        beta = f.root.com.scan_data.beta[...][0]
+        mu = f.root.com.scan_data.mu[...][0]
+        gamma = f.root.com.scan_data.gamma[...][0]
+        delta = f.root.com.scan_data.delta[...][0]
+
         try:
-            beta = f.root.com.scan_data.beta[...][0]
-            mu = f.root.com.scan_data.mu[...][0]
-            gamma = f.root.com.scan_data.gamma[...][0]
-            delta = f.root.com.scan_data.delta[...][0]
-
+            h = f.root.com.scan_data.h[...][0]
+            k = f.root.com.scan_data.k[...][0]
+            l = f.root.com.scan_data.l[...][0]
         except tb.NoSuchNodeError:
-            pass
+            h, k, l = np.nan, np.nan, np.nan
 
-        return os.path.basename(filename), beta, mu, gamma, delta
+        return os.path.basename(filename), beta, mu, gamma, delta, h, k, l
 
 
 # If used as script, iterate on glob string
@@ -60,8 +63,9 @@ if __name__ == "__main__":
         exit()
 
     # Iterate on file list
+    print("Printing the first value in the list.")
     table = PrettyTable(
-        field_names=["Filename", "Beta", "Mu", "Gamma", "Delta"],
+        field_names=["Filename", "Beta", "Mu", "Gamma", "Delta", "h", "k", "l"],
         header=True,
         float_format=".3"
     )
